44027693055e8d1655e08d3bb15ea0623d94153e
xiaoman789 <1601002656@qq.com>
2023-12-12 20:24:39 +0800

    2023.12.12 第二次

---
+++ Python/LC_15_threeSum_simple.py
@@ -0,0 +1,73 @@
+"""
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]]
+满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+你返回所有和为 0 且不重复的三元组。
+注意：答案中不可以包含重复的三元组。
+
+示例 1：
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+解释：
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+注意，输出的顺序和三元组的顺序并不重要。
+
+示例 2：
+输入：nums = [0,1,1]
+输出：[]
+解释：唯一可能的三元组和不为 0 。
+
+示例 3：
+输入：nums = [0,0,0]
+输出：[[0,0,0]]
+解释：唯一可能的三元组和为 0 。
+
+提示：
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+"""
+"""
+思路一：
+    暴力：三层for循环
+    
+"""
+
+
+# 暴力算法 超时
+def threeSum1(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    ansMap = dict()
+    answer = []
+    answer1 = []
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1,len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    ls = sorted([nums[i], nums[j], nums[k]])
+                    if ls in answer1:
+                        continue
+                    answer1.append(ls)
+                    answer.append([nums[i], nums[j], nums[k]])
+    return answer
+
+# 哈希法
+def threeSum(nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    answer = []
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            for k in range(j+1,len(nums)):
+                if nums[i] + nums[j] + nums[k] == 0:
+                    answer.append([nums[i], nums[j], nums[k]])
+    return answer
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+print(threeSum1(nums))
