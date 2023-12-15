df1363e6665df709a9b45fe4222713209bd75a6f
xiaoman789 <1601002656@qq.com>
2023-12-15 15:15:43 +0800

    2023.12.15 第一次更新

--- Python/LC_70_climbStairs_simple.py
+++ Python/LC_70_climbStairs_simple.py
@@ -24,3 +24,16 @@ def climbStairs(n):
     :type n: int
     :rtype: int
     """
+    dp = [0] * (n + 1)
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    dp[1] = 1
+    dp[2] = 2
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+n = 3
+print(climbStairs(n))
